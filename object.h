dd92a102e781aa2bfb36c258c55aaad95bb7541b
kvltst <43160474+kvltst@users.noreply.github.com>
2021-06-03 09:12:32 -0500

    Working Release 0.2.1
    First working version

---
+++ object.h
@@ -0,0 +1,16 @@
+// ball.h
+
+class Object
+{
+	public:
+		double x;
+		double y;
+		double radius;
+
+		double speed;
+		double accel;
+
+		bool isMoving;
+
+		int dir;
+};
---
+++ paddle.h
@@ -0,0 +1,15 @@
+// paddle.h
+
+class Paddle
+{
+	public:
+		double x;
+		double y;
+		double width;
+		double height;
+		
+		int dir;
+	
+	private:
+		// blah
+};
